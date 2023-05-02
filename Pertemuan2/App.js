df51ae7283b8b0afd85d0bf7d6d7a976bb90f2a7
Danilson69 <114250856+Danilson69@users.noreply.github.com>
2023-05-02 20:55:06 +0700

    Add files via upload

---
+++ Pertemuan2/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Motorcycle from './src/components/Motorcycle';
+import Car from './src/components/Car';
+import Bicycle from './src/components/bicycle';
+import Student from './src/components/Students';
+
+export default function App() {
+  return (
+<>
+<Student/>
+{/* <Car/> */}
+{/* <Bicycle/> */}
+{/* <Student/> */}
+</>
+  )
+}
+
+const styles = StyleSheet.create({
+});
